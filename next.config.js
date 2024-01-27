bb3bbf7d84b8edad031d14f69773788f0d6f2c08
Gus Shaal <ghiathchaal.beg@gmail.com>
2024-01-27 22:07:48 +0000

    Removed nx monorepo | Added Prisma | Migrated to Next 14.1 | Implemented Auth with roles and user permissions

---
+++ next.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "chrona.me",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
