d2b6620cf496532166b1266be5932bef3852e6eb
Gus Shaal <ghiathchaal.beg@gmail.com>
2024-04-12 23:51:17 +0000

    Added pipeline to push to DigitalOcean registry | Configured SMTP | Fix 5

--- app/api/auth/forgot-password/route.ts
+++ app/api/auth/forgot-password/route.ts
@@ -1,26 +1,26 @@
-import { sendEmail } from '@/lib/nodemailer'
-import DeviceDetector from 'device-detector-js'
-import { eTemplate } from '@/lib/eTemplate'
-import { NextResponse } from 'next/server'
-import { getErrorResponse, getResetPasswordToken } from '@/lib/helpers'
-import { prisma } from '@/lib/prisma.db'
+import { sendEmail } from "@/lib/nodemailer";
+import DeviceDetector from "device-detector-js";
+import { eTemplate } from "@/lib/eTemplate";
+import { NextResponse } from "next/server";
+import { getErrorResponse, getResetPasswordToken } from "@/lib/helpers";
+import { prisma } from "@/lib/prisma.db";
 
 export async function POST(req: NextApiRequestExtended) {
   try {
-    const { email } = await req.json()
-    if (!email) return getErrorResponse('Please enter your email', 400)
+    const { email } = await req.json();
+    if (!email) return getErrorResponse("Please enter your email", 400);
 
-    const host = req.headers.get('host') // localhost:3000
-    const protocol = req.headers.get('x-forwarded-proto') // http
+    const host = req.headers.get("host"); // localhost:3000
+    const protocol = req.headers.get("x-forwarded-proto"); // http
 
     const user = await prisma.user.findUnique({
       where: { email: email.toLowerCase() },
-    })
+    });
 
     if (!user)
-      return getErrorResponse(`There is no user with email ${email}`, 404)
+      return getErrorResponse(`There is no user with email ${email}`, 404);
 
-    const reset = await getResetPasswordToken()
+    const reset = await getResetPasswordToken();
 
     await prisma.user.update({
       where: { id: user.id },
@@ -28,18 +28,18 @@ export async function POST(req: NextApiRequestExtended) {
         resetPasswordToken: reset.resetPasswordToken,
         resetPasswordExpire: reset.resetPasswordExpire,
       },
-    })
+    });
 
-    const deviceDetector = new DeviceDetector()
+    const deviceDetector = new DeviceDetector();
     const device = deviceDetector.parse(
-      req.headers.get('user-agent') || ''
-    ) as any
+      req.headers.get("user-agent") || ""
+    ) as any;
 
     const {
       client: { type: clientType, name: clientName },
       os: { name: osName },
       device: { type: deviceType, brand },
-    } = device
+    } = device;
 
     const message = eTemplate({
       url: `${protocol}://${host}/auth/reset-password/${reset.resetToken}`,
@@ -49,24 +49,24 @@ export async function POST(req: NextApiRequestExtended) {
       osName,
       deviceType,
       brand,
-      webName: 'Next.JS Boilerplate',
-      validTime: '10 minutes',
-      addressStreet: 'Makka Almukarrama',
-      addressCountry: 'Mogadishu - Somalia',
-    })
+      webName: "Chrona ©",
+      validTime: "10 minutes",
+      addressStreet: "Dublin",
+      addressCountry: "Irelnad",
+    });
 
     const result = await sendEmail({
       to: user.email,
-      subject: 'Password Reset Request',
+      subject: "Password Reset Request",
       text: message,
-      webName: 'Next.JS Boilerplate Team',
-    })
+      webName: "Next.JS Boilerplate Team",
+    });
 
     if (result)
       return NextResponse.json({
         message: `An email has been sent to ${email} with further instructions.`,
-      })
+      });
   } catch ({ status = 500, message }: any) {
-    return getErrorResponse(message, status)
+    return getErrorResponse(message, status);
   }
 }
