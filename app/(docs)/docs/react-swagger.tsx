e3f2335f630272dadf849c95f5abde5c4f13345e
Gus Shaal <ghiathchaal.beg@gmail.com>
2026-02-08 03:35:45 +0000

    chore: update dependencies

--- app/(docs)/docs/react-swagger.tsx
+++ app/(docs)/docs/react-swagger.tsx
@@ -1,30 +1,17 @@
 "use client";
 
-import "@stoplight/elements/styles.min.css";
+import "swagger-ui-react/swagger-ui.css";
 import dynamic from "next/dynamic";
 
-const APIDocumentUI = dynamic(
-  () => import("@stoplight/elements").then((mod) => mod.API),
-  {
-    ssr: false,
-    loading: () => <p>Loading...</p>,
-  }
-);
+const SwaggerUI = dynamic(() => import("swagger-ui-react"), {
+  ssr: false,
+  loading: () => <p>Loading API documentation...</p>,
+});
 
 export default function ReactSwagger() {
   return (
-    <>
-      <div
-        className="min-h-screen grid grid-cols-[auto,1fr]"
-        suppressHydrationWarning
-      >
-        <div className="sidebar bg-gray-800"></div>
-        <APIDocumentUI
-          apiDescriptionUrl="/api/docs"
-          layout="sidebar"
-          router="hash"
-        />
-      </div>
-    </>
+    <div className="min-h-screen">
+      <SwaggerUI url="/api/docs" />
+    </div>
   );
 }
